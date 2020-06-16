3174d948d5047d4ca1202857c5ec4c1f66184ce8
EduViva <miissjackson72@gmail.com>
2020-06-16 10:34:39 -0300

    mail live test

--- contact.php
+++ contact.php
@@ -1,16 +1,22 @@
 <?php
 
-$arr = $_POST['message'];
+    $arr = $_POST['message'];
 
-$nome = !(empty($arr['name'])) ? $arr['name'] : "";
-$telefone = !(empty($arr['telefone'])) ? $arr['telefone'] : "";
-$msg = !(empty($arr['message'])) ? $arr['message'] : "";
-$mail = !(empty($arr['mail'])) ? $arr['mail'] : "";
-
-
-echo $nome .", ". $mail .", ". $telefone .", ". $msg;
+    $nome = !(empty($arr['name'])) ? $arr['name'] : "";
+    $telefone = !(empty($arr['telefone'])) ? $arr['telefone'] : "";
+    $msg = !(empty($arr['message'])) ? $arr['message'] : "";
+    $mail = !(empty($arr['mail'])) ? $arr['mail'] : "";
 
 
-//Se salvar no banco de dados echo true se der erro no Banco de dados echo false;
+    //echo $nome .", ". $mail .", ". $telefone .", ". $msg;
+
+    $to = "eduardovivaa@gmail.com";
+    $assunto = "Novo cadastro no site";
+    $mensagem = "Ola, você recebeu um e-mail de ". $mail . $telefone;
+    $mensagem += "--------------------------------------------------";
+    $mensagem += $msg;
+    echo mail($to,$assunto,$mensagem);
+
+    //Se salvar no banco de dados echo true se der erro no Banco de dados echo false;
 
 ?>
