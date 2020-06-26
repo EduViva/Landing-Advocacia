9a86ec5eaf422f0fcda618b70c84e7417472b106
EduViva <miissjackson72@gmail.com>
2020-06-26 11:40:34 -0300

    mail send changes

--- models/contact.php
+++ models/contact.php
@@ -1,8 +1,6 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
 
-    require '../PHPMailer/PHPMailerAutoload.php';
-    require "./dbAccess.php";
+    date_default_timezone_set('America/Sao_Paulo');
 
     $arr = $_POST['message'];
 
@@ -30,49 +28,27 @@
 
     if($local == 'news'){
     
-        date_default_timezone_set('America/Sao_Paulo');
+        $myEmail = $mailUser;//é necessário informar um e-mail do próprio domínio
+        $headers = "From: $myEmail\r\n";
+        $headers .= "Reply-To: $myEmail\r\n";
 
-        //Create a new PHPMailer instance
-        $mail = new PHPMailer;
+        /*abaixo contém os dados que serão enviados para o email
+        cadastrado para receber o formulário*/
+        $subject = "Um novo contato foi cadastrado no site";
 
-        $mail->isSMTP();
+        $corpo = "Olá!\n";
+        $corpo .= "\n";
+        $corpo .= "Seja bem Vindo\n";
+        $corpo .= "Você está cadastrado na newsletter de Fantin & Imhoff Advogados\n";
+    
+        $email_to = $email;
+        //não esqueça de substituir este email pelo seu.
+
+        $status = mail($email_to, $subject, $corpo, $headers);
+        //enviando o email.
         
-        $mail->Debugoutput = 'html';
-        
-        $mail->Host = 'smtp.umbler.com';
-        $mail->Port = 587;
-
-        $mail->SMTPSecure = 'tls';
-        $mail->SMTPAuth = true;
-
-        $mail->Username = $mailUser;
-        $mail->Password = $mailPass;
-
-        $mail->setFrom($mailUser, 'Fantin e Imhoff Advogados');
-        //Set an alternative reply-to address
-        //$mail->addReplyTo('replyto@example.com', 'First Last');
-        
-        //Set who the message is to be sent to
-        $mail->addAddress($email, 'Fantin e Imhoff');
-
-        //Set the subject line
-        $mail->Subject = 'Cadastro na newsletter de Fantin & Imhoff Advogados';
-
-        //Read an HTML message body from an external file, convert referenced images to embedded,
-        //convert HTML into a basic plain-text alternative body
-        //$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-        $mail->Body = 'Olá! Você está cadastrado na newsletter de Fantin & Imhoff Advogados.';
-        
-        //Replace the plain text body with one created manually
-        $mail->AltBody = 'Olá! Você está cadastrado na newsletter de Fantin & Imhoff Advogados.';
-
-        //Attach an image file
-        //$mail->addAttachment('images/phpmailer_mini.png');
-
-        //send the message, check for errors
-        $ret = $mail->send();
-        echo $ret;
-        if (!$ret) {
+        echo $status;
+        if (!$status) {
            $sql = "UPDATE `newsletter` SET enviado = 'Não' WHERE id = '". $lastId . "'";
         } else {
             $sql = "UPDATE `newsletter` SET enviado = 'Sim' WHERE id = '". $lastId . "'";
